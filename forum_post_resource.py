a7ddf55fdc530aecf1532b10de7cf326d0ab364a
Brian Lee <the930325@yahoo.com>
2022-10-25 15:10:13 -0400

    add post add response

--- forum_post_resource.py
+++ forum_post_resource.py
@@ -54,7 +54,7 @@ class ForumPostResource:
     def get_posts_by_label(user_id, label):
         ## return posts
         sql1 = """
-            SELECT P.Post_ID, P.Title, P.User_ID, P.Time, L.name AS Location, P.Label, count(T.PT_ID) AS Thumbs, 
+            SELECT P.Post_ID, P.Title, P.User_ID, P.Time, L.Name AS Location, P.Label, count(T.PT_ID) AS Thumbs, 
                 if(U.Post_ID is null, false, true) AS is_Thumbed
             FROM ms3.Post P
                 LEFT JOIN ms3.Location L ON P.Location_ID = L.Location_ID
@@ -70,7 +70,7 @@ class ForumPostResource:
         conn = ForumPostResource._get_connection()
         cur = conn.cursor()
         try:
-            cur.execute(sql1, args=(user_id, label))
+            cur.execute(sql1 % (user_id, label))
             # if success
             res = cur.fetchall()
             if res:
@@ -102,7 +102,7 @@ class ForumPostResource:
         """
         cur = conn.cursor()
         try:
-            cur.execute(sql2, args=(user_id, label))
+            cur.execute(sql2 % (user_id, label))
             # if success
             res = cur.fetchall()
             if res:
@@ -120,7 +120,7 @@ class ForumPostResource:
 
         ## return post details
         sql1 = """
-            SELECT P.* , L.name AS Location, L.Map_URL, count(T.PT_id) AS Thumbs, if(U.PT_ID is null, false, true) AS is_Thumbed
+            SELECT P.* , L.Name AS Location, L.Map_URL, count(T.PT_id) AS Thumbs, if(U.PT_ID is null, false, true) AS is_Thumbed
             FROM ms3.Post P
                 LEFT JOIN ms3.Post_Thumbs T ON P.Post_id = T.Post_id
                 LEFT JOIN (SELECT * FROM ms3.Post_Thumbs WHERE User_ID= %s) U ON P.Post_ID = U.Post_ID
@@ -129,7 +129,7 @@ class ForumPostResource:
             """
         cur = conn.cursor()
         try:
-            cur.execute(sql1, args=(user_id, post_id))
+            cur.execute(sql1 % (user_id, post_id))
             # if success
             res = cur.fetchall()
             if res:
@@ -151,7 +151,7 @@ class ForumPostResource:
         """
         cur = conn.cursor()
         try:
-            cur.execute(sql2, args=(user_id, post_id))
+            cur.execute(sql2 % (user_id, post_id))
             # if success
             res = cur.fetchall()
             if res:
@@ -171,7 +171,7 @@ class ForumPostResource:
 
         ## return post details
         sql1 = """
-            SELECT P.Post_ID, P.Title, P.User_ID, P.Time, L.name AS Location, P.Label, count(T.PT_ID) AS Thumbs,
+            SELECT P.Post_ID, P.Title, P.User_ID, P.Time, L.Name AS Location, P.Label, count(T.PT_ID) AS Thumbs,
                 if(U.Post_ID is null, false, true) AS is_Thumbed
             FROM ms3.Post P
                 LEFT JOIN ms3.Location L ON P.Location_ID = L.Location_ID
@@ -181,12 +181,12 @@ class ForumPostResource:
                     FROM ms3.Post_Thumbs
                     WHERE User_ID = %s
                 ) U ON P.Post_ID = U.Post_ID
-            WHERE P.Author = %s
+            WHERE P.User_ID = %s
             GROUP BY P.Post_id;
         """
         cur = conn.cursor()
         try:
-            cur.execute(sql1, args=(user_id, user_id))
+            cur.execute(sql1 % (user_id, user_id))
             # if success
             res = cur.fetchall()
             if res:
@@ -209,16 +209,16 @@ class ForumPostResource:
                     WHERE User_ID = %s
                 ) U ON R.Response_ID = U.Response_ID
                 LEFT JOIN (
-                    SELECT Post_ID, Author
+                    SELECT Post_ID, User_ID
                     FROM ms3.Post
-                    WHERE Author = %s
+                    WHERE User_ID = %s
                 ) L ON R.Post_ID = L.Post_ID
             GROUP BY R.Response_ID
             HAVING mypost = TRUE;
         """
         cur = conn.cursor()
         try:
-            cur.execute(sql2, args=(user_id, user_id))
+            cur.execute(sql2 % (user_id, user_id))
             # if success
             res = cur.fetchall()
             if res:
@@ -233,16 +233,26 @@ class ForumPostResource:
 
     def add_post(user_id, title, location, label, content):
         t = str(datetime.now())
-        sql = "INSERT INTO ms3.Post (Title, Author, Time, Location_ID, Label, Content) VALUES (%s,%s,%s,%s,%s,%s)"
+        sql_query = "SELECT COUNT(Post_ID) FROM ms3.Post;"
         conn = ForumPostResource._get_connection()
         cur = conn.cursor()
         try:
-            cur.execute(sql, args=(title, user_id, t, location, label, content))
-            res = cur.fetchall()
-            if res:
-                result = {'success': True, 'data': res}
+            cur.execute(sql_query)
+            key, val1 = next(iter(cur.fetchone().items()))
+            if location == 'None' and label == 'None':
+                cur.execute("INSERT INTO ms3.Post (Title, User_ID, Time, Content) VALUES (\'%s\', %s, \'%s\', \'%s\';" % (title, user_id, t, content))
+            elif location == 'None' and label != 'None':
+                cur.execute("INSERT INTO ms3.Post (Title, User_ID, Time, Label, Content) VALUES (\'%s\', %s, \'%s\', \'%s\', \'%s\');" % (title, user_id, t, label, content))
+            elif location != 'None' and label == 'None':
+                cur.execute("INSERT INTO ms3.Post (Title, User_ID, Time, Location_ID, Content) VALUES (\'%s\', %s, \'%s\', %d, \'%s\');" % (title, user_id, t, int(location), content))
             else:
-                result = {'success': False, 'message': 'Not Found', 'data': res}
+                cur.execute("INSERT INTO ms3.Post (Title, User_ID, Time, Location_ID, Label, Content) VALUES (\'%s\', %s, \'%s\', %d, \'%s\', \'%s\');" % (title, user_id, t, int(location), label, content))
+            cur.execute(sql_query)
+            key, val2 = next(iter(cur.fetchone().items()))
+            if val2 - val1 == 1:
+                result = {'success': True, 'message': 'post added'}
+            else:
+                result = {'success': False, 'message': 'post not added'}
         except pymysql.Error as e:
             print(e)
             result = {'success': False, 'message': str(e)}
@@ -250,19 +260,37 @@ class ForumPostResource:
 
     def add_response(user_id, post_id, content):
         t = str(datetime.now())
-        sql = "INSERT INTO ms3.Response (Post_ID, Author, Time, Content) VALUES (%s,%s,%s,%s)"
+        sql_query_post = "SELECT Title FROM ms3.Post WHERE Post_ID = %s;"
+        sql_query_resp = "SELECT COUNT(Response_ID) FROM ms3.Response WHERE Post_ID = %s;"
+        sql_insert = "INSERT INTO ms3.Response (Post_ID, User_ID, Time, Content) VALUES (%s, %s, \'%s\', \'%s\');"
         conn = ForumPostResource._get_connection()
         cur = conn.cursor()
+
         try:
-            cur.execute(sql, args=(post_id, user_id, t, content))
+            cur.execute(sql_query_post % post_id)
             res = cur.fetchall()
             if res:
-                result = {'success': True, 'data': res}
+                post_result = {'success': True, 'message': 'post found'}
             else:
-                result = {'success': False, 'message': 'Not Found', 'data': res}
+                post_result = {'success': False, 'message': 'post not found, cannot add response'}
+        except pymysql.Error as e:
+            print(e)
+            post_result = {'success': False, 'message': str(e)}
+
+        try:
+            cur.execute(sql_query_resp % post_id)
+            key, val1 = next(iter(cur.fetchone().items()))
+            cur.execute(sql_insert % (post_id, user_id, t, content))
+            cur.execute(sql_query_resp % post_id)
+            key, val2 = next(iter(cur.fetchone().items()))
+            if val2 - val1 == 1:
+                resp_result = {'success': True, 'message': 'response added to post'}
+            else:
+                resp_result = {'success': False, 'message': 'post found but response not added'}
         except pymysql.Error as e:
             print(e)
             resp_result = {'success': False, 'message': str(e)}
+
         return {'post': post_result, 'response': resp_result}
 
     def update_response(user_id, response_id, content):
@@ -343,16 +371,16 @@ class ForumPostResource:
         conn = ForumPostResource._get_connection()
         cur = conn.cursor()
         try:
-            cur.execute(sql_p, args=(post_id, user_id))
+            cur.execute(sql_p % (post_id, user_id))
             res = cur.fetchall()
             # if already thumbed
             if res:
                 sql = "DELETE FROM ms3.Post_Thumbs WHERE Post_ID = %s AND User_ID = %s;"
-                cur.execute(sql, args=(post_id, user_id))
+                cur.execute(sql % (post_id, user_id))
                 result = {'success': True, 'message': 'Thumb deleted to post'}
             else:
-                sql = "INSERT INTO ms3.Post_Thumbs VALUES (DEFAULT,%s,%s)"
-                cur.execute(sql, args=(post_id, user_id))
+                sql = "INSERT INTO ms3.Post_Thumbs VALUES (DEFAULT,%s,%s);"
+                cur.execute(sql % (post_id, user_id))
                 result = {'success': True, 'message': 'Thumb added to post'}
         except pymysql.Error as e:
             print(e)
